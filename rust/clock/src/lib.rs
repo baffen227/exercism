ab6d3c6ecbb572f31c2d123d880c692dc1dd1b34
baffen227 <baffen227@gmail.com>
2023-11-22 12:40:10 +0800

    Implement Display trait for struct Clock
    test functions "on_the_hour" and "past_the_hour" passed

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,11 +1,25 @@
-pub struct Clock;
+use std::fmt;
+
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        Clock {
+            hours,
+            minutes,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         todo!("Add {minutes} minutes to existing Clock time");
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+         write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
     }
 }
