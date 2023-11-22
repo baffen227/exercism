2b4620c4df188beddb49a19a09038ce5e2cd1378
baffen227 <baffen227@gmail.com>
2023-11-22 17:45:11 +0800

    Refine Clock constructor
    To pass the following test functions:
    negative_hour
    negative_hour_roll_over
    negative_hour_roll_over_continuously

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -7,9 +7,14 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
+        let hours = if hours >= 0 { hours } else {
+            (hours % 24) + 24
+        };
+
         let total_minutes = hours * 60 + minutes;
         let hours = total_minutes / 60;
         let minutes = total_minutes % 60;
+
         Clock {
             hours: if hours >= 24 { hours % 24 } else { hours },
             minutes,
