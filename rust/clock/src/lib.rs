5eacc19e37e293da17d7cf63fef36ab2af524a5e
baffen227 <baffen227@gmail.com>
2023-11-24 17:22:42 +0800

    Update Clock struct to contain minutes field only
    And update related methods

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,29 +1,30 @@
 use std::fmt;
+const MINUTES_PER_DAY: i32 = 1440;
+const MINUTES_PER_HOUR: i32 = 60;
 
 #[derive(PartialEq, Debug)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl Clock {
-    const A_DAY_IN_MINUTES: i32 = 24 * 60;
-
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let total = (hours * 60 + minutes).rem_euclid(Self::A_DAY_IN_MINUTES);
-        Clock {
-            hours: total / 60,
-            minutes: total % 60,
+        Self {
+            minutes: (hours * MINUTES_PER_HOUR + minutes).rem_euclid(MINUTES_PER_DAY),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Self::new(self.hours, self.minutes + minutes)
+        Self {
+            minutes: (self.minutes + minutes).rem_euclid(MINUTES_PER_DAY),
+        }
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-         write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
+         write!(f, "{:02}:{:02}",
+            self.minutes / MINUTES_PER_HOUR,
+            self.minutes % MINUTES_PER_HOUR)
     }
 }
