1b21e749f49bf0ff782ef6d51386077916db1c3c
baffen227 <baffen227@gmail.com>
2023-11-29 22:32:08 +0800

    Implement years_during method of every planets

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -42,9 +42,41 @@ impl Planet for Venus {
         (d.seconds as f64 / 31557600.0) / 0.61519726
     }
 }
+
 impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+
+impl Planet for Mars {
+    fn years_during(d: &Duration) -> f64 {
+        // Mars: orbital period 1.8808158 Earth years
+        // 1 Earth year = 1 / 1.8808158
+        (d.seconds as f64 / 31557600.0) / 1.8808158
+    }
+}
+
+impl Planet for Jupiter {
+    fn years_during(d: &Duration) -> f64 {
+        // Jupiter: orbital period 11.862615 Earth years
+        (d.seconds as f64 / 31557600.0) / 11.862615
+    }
+}
+
+impl Planet for Saturn {
+    fn years_during(d: &Duration) -> f64 {
+        // Saturn: orbital period 29.447498 Earth years
+        (d.seconds as f64 / 31557600.0) / 29.447498
+    }
+}
+
+impl Planet for Uranus {
+    fn years_during(d: &Duration) -> f64 {
+        // Uranus: orbital period 84.016846 Earth years
+        (d.seconds as f64 / 31557600.0) / 84.016846
+    }
+}
+
+impl Planet for Neptune {
+    fn years_during(d: &Duration) -> f64 {
+        // Neptune: orbital period 164.79132 Earth years
+        (d.seconds as f64 / 31557600.0) / 164.79132
+    }
+}
