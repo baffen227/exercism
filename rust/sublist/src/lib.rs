b35881f6b8f2e2b818c5a93c34d8f328b8f972d1
baffen227 <baffen227@gmail.com>
2024-05-01 20:56:48 +0800

    Update solution to exercise "Sublist"

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,51 +7,30 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    //todo!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
     if _first_list == _second_list {
         Comparison::Equal
     } else {
-        let _first_len = _first_list.len();
-        let _second_len = _second_list.len();
-
-
-        /*
-        let mut longer_len = 0;
-        if _first_len <= _second_len {
-            longer_len = _second_len;
-        } else {
-            longer_len = _first_len;
+        let mut shorter_list = _first_list;
+        let mut longer_list = _second_list;
+        if _first_list.len() > _second_list.len() {
+            shorter_list = _second_list;
+            longer_list = _first_list;
         }
-        */
 
-
-        // TODO: refine the flow
-        if _first_len <= _second_len {
-            let mut start_idx = 0;
-            let mut end_idx = start_idx + _first_len;
-            while end_idx <= _second_len {
-                if _first_list == &_second_list[start_idx..end_idx] {
+        let mut start_idx = 0;
+        let mut end_idx = start_idx + shorter_list.len();
+        while end_idx <= longer_list.len() {
+            if shorter_list == &longer_list[start_idx..end_idx] {
+                if shorter_list == _first_list {
                     return Comparison::Sublist;
                 } else {
-                    start_idx += 1;
-                    end_idx = start_idx + _first_len;
-                }
-            }
-            return Comparison::Unequal;
-
-        } else {
-            let mut start_idx = 0;
-            let mut end_idx = start_idx + _second_len;
-            while end_idx <= _first_len {
-                if _second_list == &_first_list[start_idx..end_idx] {
                     return Comparison::Superlist;
-                } else {
-                    start_idx += 1;
-                    end_idx = start_idx + _second_len;
                 }
+            } else {
+                start_idx += 1;
+                end_idx = start_idx + shorter_list.len(); 
             }
-            return Comparison::Unequal;
         }
-
+        return Comparison::Unequal;
     }
 }
