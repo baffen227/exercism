fecf93480d845e7f831402f2ead287881a5e6313
Harry Chen <baffen227@gmail.com>
2024-09-27 09:51:07 +0800

    Update lib.rs in exercise minsweeper

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,3 +1,46 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    todo!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:#?}\n");
+	struct MindfieldDim {
+		row_num: u8,
+		column_num: u8,
+	}
+
+	enum InitialStatus {
+		NoRows,
+		NoColumns,
+		Invalid,
+		Valid(MindfieldDim),
+	}
+
+	fn verify(minefield: &[&str]) -> InitialStatus {
+		let row_num = minefield.len();
+		if row_num == 0 {
+			InitialStatus::NoRows
+		} else if minefield[0].is_empty() {
+			InitialStatus::NoColumns
+		} else {
+			let column_num = minefield[0].len();
+			if minefield
+				.iter()
+				.any(|s| s.len() != column_num)
+			{
+				InitialStatus::Invalid
+			} else {
+				InitialStatus::Valid(MindfieldDim {
+					row_num: row_num as u8,
+					column_num: column_num as u8,
+				})
+			}
+		}
+	}
+
+	fn do_annotate(minefield: &[&str], dim: MindfieldDim) -> Vec<String> {
+		minefield.iter().flatten()
+	}
+
+	match verify(minefield) {
+		InitialStatus::NoRows => Vec::new(),
+		InitialStatus::NoColumns => vec!["".to_string()],
+		InitialStatus::Invalid => Vec::new(),
+		InitialStatus::Valid(dim) => do_annotate(minefield, dim),
+	}
 }
