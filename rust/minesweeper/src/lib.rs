bca4ce1ba87e38707196b780e795be86982880bc
Harry Chen <baffen227@gmail.com>
2024-09-27 10:56:19 +0800

    Update lib.rs in minsweeper

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,14 +1,16 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-	struct MindfieldDim {
+	use std::collections::HashMap;
+	struct MinefieldMap {
 		row_num: u8,
 		column_num: u8,
+		field_map: HashMap<(u8, u8), char>,
 	}
 
 	enum InitialStatus {
 		NoRows,
 		NoColumns,
 		Invalid,
-		Valid(MindfieldDim),
+		Valid(MinefieldMap),
 	}
 
 	fn verify(minefield: &[&str]) -> InitialStatus {
@@ -25,22 +27,31 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
 			{
 				InitialStatus::Invalid
 			} else {
-				InitialStatus::Valid(MindfieldDim {
+				let mut field_map = HashMap::<(u8, u8), char>::new();
+				for (idx, c) in minefield
+					.iter()
+					.flat_map(|s| s.chars())
+					.enumerate()
+				{
+					let i = (idx / column_num) as u8;
+					let j = (idx % column_num) as u8;
+					field_map.insert((i, j), c);
+				}
+				InitialStatus::Valid(MinefieldMap {
 					row_num: row_num as u8,
 					column_num: column_num as u8,
+					field_map,
 				})
 			}
 		}
 	}
 
-	fn do_annotate(minefield: &[&str], dim: MindfieldDim) -> Vec<String> {
-		minefield.iter().flatten()
-	}
+	fn do_annotate(minefield_map: MinefieldMap) -> Vec<String> {}
 
 	match verify(minefield) {
 		InitialStatus::NoRows => Vec::new(),
 		InitialStatus::NoColumns => vec!["".to_string()],
 		InitialStatus::Invalid => Vec::new(),
-		InitialStatus::Valid(dim) => do_annotate(minefield, dim),
+		InitialStatus::Valid(minefield_map) => do_annotate(minefield_map),
 	}
 }
