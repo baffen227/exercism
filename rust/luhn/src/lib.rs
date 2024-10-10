3fc8d6f5330caee52235f7672859ec1ae10bd07f
Harry Chen <baffen227@gmail.com>
2024-10-10 17:13:12 +0800

    Finish exercise luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,10 +1,28 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-	let iter1 = code
+	let iter = code
 		.chars()
-		.filter(|c| !c.is_whitespace());
-	(iter1.clone().count() >= 2)
-		&& (iter1
+		.filter(|c| !c.is_whitespace())
+		.rev();
+
+	(iter.clone().count() >= 2)
+		&& (iter
 			.clone()
 			.all(|c| c.is_ascii_digit()))
+		&& (iter
+			.map(|c| c.to_digit(10).unwrap())
+			.enumerate()
+			.map(|(idx, d)| {
+				if idx % 2 == 1 {
+					if d * 2 > 9 {
+						d * 2 - 9
+					} else {
+						d * 2
+					}
+				} else {
+					d
+				}
+			})
+			.sum::<u32>()
+			% 10 == 0)
 }
