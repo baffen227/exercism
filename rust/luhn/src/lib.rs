1be15a4b0d181bc85b3f820ac3b063ffc7667c1c
Harry Chen <baffen227@gmail.com>
2024-10-10 13:45:55 +0800

    Fix iterator issue by clone()

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,8 +1,10 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-	let mut iter1 = code
+	let iter1 = code
 		.chars()
 		.filter(|c| !c.is_whitespace());
-
-	iter1.all(|c| c.is_ascii_digit()) && iter1.count() >= 2
+	(iter1.clone().count() >= 2)
+		&& (iter1
+			.clone()
+			.all(|c| c.is_ascii_digit()))
 }
