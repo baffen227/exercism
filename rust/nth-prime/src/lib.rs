f222c8c49c06647cd0ed8ed703360c7e559d9b5b
Harry Chen <baffen227@gmail.com>
2024-10-14 17:47:27 +0800

    Update lib.rs in nth-prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,5 @@
 pub fn nth(n: u32) -> u32 {
-    todo!("What is the 0-indexed {n}th prime number?")
+	todo!("What is the 0-indexed {n}th prime number?")
+	// refer to https://www.geeksforgeeks.org/program-to-find-the-nth-prime-number/
+	// use singleton pattern
 }
