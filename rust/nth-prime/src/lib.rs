d160e84bbeb4fb95637fa23c154df2425fc60521
Harry Chen <baffen227@gmail.com>
2024-10-15 15:37:47 +0800

    Update lib.rs in nth-prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,5 +1,35 @@
+use lazy_static::lazy_static;
+
+lazy_static! {
+	static ref primes: Vec<u32> = {
+		const MAX_SIZE: usize = 1000005;
+		let mut is_prime = [true; MAX_SIZE];
+		let mut p = 2;
+		while p * p < MAX_SIZE {
+			if is_prime[p] {
+				let mut i = p * p;
+				while i < MAX_SIZE {
+					is_prime[i] = false;
+					i += p;
+				}
+			}
+			p += 1;
+		}
+
+		let mut v: Vec<u32> = vec![];
+		p = 2;
+		while p < MAX_SIZE {
+			if is_prime[p] {
+				v.push(p as u32);
+			}
+			p += 1;
+		}
+		v
+	};
+}
+
 pub fn nth(n: u32) -> u32 {
-	todo!("What is the 0-indexed {n}th prime number?")
-	// refer to https://www.geeksforgeeks.org/program-to-find-the-nth-prime-number/
-	// use singleton pattern
+	*(primes
+		.get(n as usize)
+		.unwrap())
 }
