47b076d251e3fc544ac1f403fd56eb7b8ae51a8d
Harry Chen <baffen227@gmail.com>
2024-11-04 16:03:07 +0800

    Update lib.rs in prime-factors

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,32 @@
+#![feature(isqrt)]
+
 pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+	let mut res = vec![];
+	let mut n = n;
+
+	// push the number of 2s that divide n
+	while n % 2 == 0 {
+		res.push(2);
+		n /= 2;
+	}
+
+	// n must be odd at this point. So we can skip
+	// one element (Note i = i + 2)
+	let mut i = 3;
+	while i <= n.isqrt() {
+		// while i divides n, push i and divide n
+		while n % i == 0 {
+			res.push(i);
+			n /= i;
+		}
+		i += 2;
+	}
+
+	// This condition is to handle the case when n
+	// is a prime number greater than 2
+	if n > 2 {
+		res.push(n);
+	}
+
+	res
 }
