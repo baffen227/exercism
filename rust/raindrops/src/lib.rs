4d2d3cf6150fd44cadcd9770a59d5cc253a031da
Harry Chen <baffen227@gmail.com>
2024-11-19 16:25:08 +0800

    Update lib.rs in raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn raindrops(n: u32) -> String {
-    todo!("what sound does Raindrop #{n} make?")
+	if (n % 3 != 0) && (n % 5 != 0) && (n % 7 != 0) {
+		n.to_string()
+	} else {
+		let mut res = vec![];
+		if n % 3 == 0 {
+			res.push("Pling");
+		}
+		if n % 5 == 0 {
+			res.push("Plang");
+		}
+		if n % 7 == 0 {
+			res.push("Plong");
+		}
+		res.join("")
+	}
 }
