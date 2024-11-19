49dff4f885e1d937045524e5904a860713a55074
Harry Chen <baffen227@gmail.com>
2024-11-19 14:19:31 +0800

    Update lib.rs in proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    todo!("build a proverb from this list of items: {list:?}")
+	let mut result = vec![];
+	let mut iter = list.iter().peekable();
+	while let Some(first) = iter.next() {
+		match iter.peek() {
+			Some(second) => {
+				let s = format!("For want of a {} the {} was lost.", first, second);
+				result.push(s);
+			}
+			None => {
+				let s = format!("And all for the want of a {}.", list[0]);
+				result.push(s);
+			}
+		}
+	}
+	result.join("\n")
 }
