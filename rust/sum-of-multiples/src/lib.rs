8ed8b28759c73faab4e28461d43c8d4bd8322970
Harry Chen <baffen227@gmail.com>
2024-11-21 17:30:53 +0800

    Update lib.rs in sum-of-multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,3 +1,22 @@
+use std::collections::BTreeSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+	let mut results: BTreeSet<u32> = BTreeSet::new();
+	for factor in factors {
+		if *factor != 0 {
+			let mut multiples = BTreeSet::new();
+			let mut n = 1;
+			let mut multiple = factor * n;
+			while multiple < limit {
+				multiples.insert(multiple);
+				n += 1;
+				multiple = factor * n;
+			}
+			results = results
+				.union(&multiples)
+				.cloned()
+				.collect();
+		}
+	}
+	results.iter().sum::<u32>()
 }
