d9a4713e081f4b300c97762eb115e9d2a89212ef
Harry Chen <baffen227@gmail.com>
2024-11-22 15:40:09 +0800

    Refine lib.rs in sum-of-multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,18 +1,17 @@
 use std::collections::BTreeSet;
 
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-	let mut results: BTreeSet<u32> = BTreeSet::new();
-	factors
+	let results: BTreeSet<u32> = factors
 		.iter()
 		.filter(|i| **i != 0)
-		.for_each(|factor| {
+		.fold(BTreeSet::new(), |results, factor| {
 			let multiples: BTreeSet<u32> = (*factor..limit)
 				.step_by(*factor as usize)
 				.collect();
-			results = results
+			results
 				.union(&multiples)
 				.cloned()
-				.collect();
+				.collect()
 		});
 	results.iter().sum::<u32>()
 }
