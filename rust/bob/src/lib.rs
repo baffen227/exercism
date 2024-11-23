f4e9e34fbe3da5ad58d926228e6377567a0c4dca
baffen227 <baffen227@gmail.com>
2024-11-23 18:52:55 +0800

    Update lib.rs in bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn reply(message: &str) -> &str {
-    todo!("have Bob reply to the incoming message: {message}")
+	let message = message.trim();
+	if message.ends_with("?") {
+		"Sure."
+	} else if message
+		.chars()
+		.filter(|c| (*c).is_alphanumeric())
+		.all(char::is_uppercase)
+	{
+		"Whoa, chill out!"
+	} else {
+		"Whatever."
+	}
 }
