e6a797f60fe05b8df8043ebd26949c9fb145cf2c
Harry Chen <baffen227@gmail.com>
2024-11-26 17:49:19 +0800

    Update lib.rs in high-scores

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -1,24 +1,36 @@
+use std::collections::BinaryHeap;
+
 #[derive(Debug)]
-pub struct HighScores;
+pub struct HighScores {
+	vector: Vec<u32>,
+	heap: BinaryHeap<u32>,
+}
 
 impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        todo!("Construct a HighScores struct, given the scores: {scores:?}")
-    }
+	pub fn new(scores: &[u32]) -> Self {
+		HighScores {
+			vector: scores.to_vec(),
+			heap: BinaryHeap::from(scores.to_vec()),
+		}
+	}
 
-    pub fn scores(&self) -> &[u32] {
-        todo!("Return all the scores as a slice")
-    }
+	pub fn scores(&self) -> &[u32] {
+		&self.vector
+	}
 
-    pub fn latest(&self) -> Option<u32> {
-        todo!("Return the latest (last) score")
-    }
+	pub fn latest(&self) -> Option<u32> {
+		self.vector.last().copied()
+	}
 
-    pub fn personal_best(&self) -> Option<u32> {
-        todo!("Return the highest score")
-    }
+	pub fn personal_best(&self) -> Option<u32> {
+		self.heap.peek().copied()
+	}
 
-    pub fn personal_top_three(&self) -> Vec<u32> {
-        todo!("Return 3 highest scores")
-    }
+	pub fn personal_top_three(&self) -> Vec<u32> {
+		let result = self.heap.clone().into_vec();
+		match result.len() {
+			0..=2 => result,
+			_ => result[0..=2].to_vec(),
+		}
+	}
 }
