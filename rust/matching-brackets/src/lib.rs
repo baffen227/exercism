a1d0b114bf0cc0414679dbd37bb23995fac1b061
Harry Chen <baffen227@gmail.com>
2024-11-27 13:16:17 +0800

    Update lib.rs in matching-brackets

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    todo!("Check if the string \"{string}\" contains balanced brackets");
+	let mut stack: Vec<char> = Vec::new();
+	for c in string
+		.chars()
+		.filter(|c| *c == '{' || *c == '}' || *c == '[' || *c == ']' || *c == '(' || *c == ')')
+	{
+		if c == '{' || c == '[' || c == '(' {
+			stack.push(c);
+		}
+		if c == '}' {
+			// if stack is empty, return false
+			// if stack is not empty, check if stack.last is '{',
+			//
+		}
+	}
+	stack.is_empty()
 }
