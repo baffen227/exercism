e9418622231d361845e3439382330913dc145c8e
Harry Chen <baffen227@gmail.com>
2024-11-27 17:52:20 +0800

    Update lib.rs in matching-brackets

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,12 +1,33 @@
+use std::collections::HashMap;
+
 pub fn brackets_are_balanced(string: &str) -> bool {
+	let brackets_map = HashMap::from([('{', '}'), ('[', ']'), ('(', ')')]);
 	let mut stack: Vec<char> = Vec::new();
 	for c in string.chars() {
-		if "{[(".contains(c) {
+		// if c is left bracket, then stack.push(c).
+		if brackets_map
+			.keys()
+			.any(|&k| k == c)
+		{
 			stack.push(c);
 		}
-		if "}])".contains(c) && stack.is_empty() {
-			return false;
+		// if c is right bracket and stack is empty, then return false.
+		if brackets_map
+			.values()
+			.any(|&v| v == c)
+		{
+			if stack.is_empty() {
+				return false;
+			} else {
+				// TODO
+				// if stack.last().copied().unwrap() == brackets {
+				// stack.pop();
+				// } else {
+				// return false;
+				// }
+			}
 		}
+
 		if c == '}' {
 			if stack.last().copied().unwrap() == '{' {
 				stack.pop();
