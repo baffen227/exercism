352e9bfbc84d5ac79f6d619811b4d9a496317348
Harry Chen <baffen227@gmail.com>
2024-11-28 09:47:47 +0800

    Update lib.rs in matching-brackets

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,54 +1,37 @@
 use std::collections::HashMap;
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-	let brackets_map = HashMap::from([('{', '}'), ('[', ']'), ('(', ')')]);
+	// Map key is right bracket, and map value is corresponding left bracket.
+	let brackets_map = HashMap::from([('}', '{'), (']', '['), (')', '(')]);
+
+	// Make use of the FILO feature of stack, the last element is top of the stack.
 	let mut stack: Vec<char> = Vec::new();
+
 	for c in string.chars() {
-		// if c is left bracket, then stack.push(c).
+		// if c is left bracket, then just stack.push(c).
 		if brackets_map
-			.keys()
+			.values()
 			.any(|&k| k == c)
 		{
 			stack.push(c);
 		}
-		// if c is right bracket and stack is empty, then return false.
+
+		// if c is right bracket,
+		//   if stack is not empty, and top of stack is corresponding left bracket of c,
+		//     stack.pop()
+		//   else
+		//     return false
 		if brackets_map
-			.values()
+			.keys()
 			.any(|&v| v == c)
 		{
-			if stack.is_empty() {
-				return false;
-			} else {
-				// TODO
-				// if stack.last().copied().unwrap() == brackets {
-				// stack.pop();
-				// } else {
-				// return false;
-				// }
-			}
-		}
-
-		if c == '}' {
-			if stack.last().copied().unwrap() == '{' {
-				stack.pop();
-			} else {
-				return false;
-			}
-		}
-		if c == ']' {
-			if stack.last().copied().unwrap() == '[' {
-				stack.pop();
-			} else {
-				return false;
-			}
-		}
-		if c == ')' {
-			if stack.last().copied().unwrap() == '(' {
+			if !stack.is_empty() && stack.last().copied().unwrap() == brackets_map[&c] {
 				stack.pop();
 			} else {
 				return false;
 			}
 		}
 	}
+
 	stack.is_empty()
 }
