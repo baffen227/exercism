aab649a8ade4c3eaa35deba1edfbbf204f2ec34e
Harry Chen <baffen227@gmail.com>
2024-11-29 13:37:27 +0800

    Update lib.rs in collatz-conjecture

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    todo!("return Some(x) where x is the number of steps required to reach 1 starting with {n}")
+	let mut i: u64 = n;
+	let mut cnt: u64 = 0;
+
+	if i == 0 {
+		return None;
+	}
+
+	while i > 1 {
+		if i % 2 == 0 {
+			i /= 2;
+		} else {
+			i = i * 3 + 1;
+		}
+		cnt += 1;
+	}
+
+	Some(cnt)
 }
