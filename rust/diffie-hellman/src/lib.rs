5e00d55e7f48fde2a5587a3bbb6204dbc93897ce
Harry Chen <baffen227@gmail.com>
2024-12-03 14:06:22 +0800

    Update lib.rs in diffie-hellman

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -1,11 +1,15 @@
+/// Pick a private key greater than 1 and less than {p}.
 pub fn private_key(p: u64) -> u64 {
-    todo!("Pick a private key greater than 1 and less than {p}")
+	p - 1
 }
 
+/// Calculate public key using prime numbers {p} and {g}, and private key {a}
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    todo!("Calculate public key using prime numbers {p} and {g}, and private key {a}")
+	g.pow(a as u32) % p
 }
 
+/// Calculate secret key using prime number {p}, public key {b_pub}, and private
+/// key {a}
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    todo!("Calculate secret key using prime number {p}, public key {b_pub}, and private key {a}")
+	b_pub.pow(a as u32) % p
 }
