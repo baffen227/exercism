e8892ff2d9dbc1587a731a78decd634a1f421cc4
Harry Chen <baffen227@gmail.com>
2024-12-04 13:47:16 +0800

    Update lib.rs in deffie-hellman

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -2,20 +2,18 @@ use num_bigint::BigUint;
 
 /// Pick a private key greater than 1 and less than {p}.
 pub fn private_key(p: u64) -> u64 {
-	p - 1
+	(p + 1) / 2
 }
 
 /// Calculate public key using prime numbers {p} and {g}, and private key {a}
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-	BigUint::from(g)
-		.modpow(&BigUint::from(a), &BigUint::from(p))
+	let (b_p, b_g, b_a) = (BigUint::from(p), BigUint::from(g), BigUint::from(a));
+	b_g.modpow(&b_a, &b_p)
 		.to_u64_digits()[0]
 }
 
 /// Calculate secret key using prime number {p}, public key {b_pub}, and private
 /// key {a}
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-	BigUint::from(b_pub)
-		.modpow(&BigUint::from(a), &BigUint::from(p))
-		.to_u64_digits()[0]
+	public_key(p, b_pub, a)
 }
