0e18c867cab87496e11e0becb249445d20ac3b8b
Harry Chen <baffen227@gmail.com>
2024-12-16 17:57:07 +0800

    Update lib.rs in kindergarten-garden

--- rust/kindergarten-garden/src/lib.rs
+++ rust/kindergarten-garden/src/lib.rs
@@ -1,28 +1,9 @@
 use std::collections::HashMap;
 
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-	let student_idx = HashMap::from([
-		("Alice", 0),
-		("Bob", 1),
-		("Charlie", 2),
-		("David", 3),
-		("Eve", 4),
-		("Fred", 5),
-		("Ginny", 6),
-		("Harriet", 7),
-		("Ileana", 8),
-		("Joseph", 9),
-		("Kincaid", 10),
-		("Larry", 11),
-	]);
-
-	let plant_names = HashMap::from([
-		('G', "grass"),
-		('C', "clover"),
-		('R', "radishes"),
-		('V', "violets"),
-	]);
-
+	// `diagram_vector` is a vector with two string.
+	// The first string contains encoded plant characters on the first row.
+	// The second string contains encoded plant characters on the second row.
 	let mut diagram_vector = Vec::new();
 	diagram
 		.split('\n')
@@ -36,12 +17,41 @@ pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
 			diagram_vector.push(row_vector);
 		});
 
+	// key: encoded plant character
+	// value: plant full name
+	let plant_names = HashMap::from([
+		('G', "grass"),
+		('C', "clover"),
+		('R', "radishes"),
+		('V', "violets"),
+	]);
+
+	// key: student name
+	// value: the index corresponding to the student
+	let name2idx = HashMap::from([
+		("Alice", 0),
+		("Bob", 1),
+		("Charlie", 2),
+		("David", 3),
+		("Eve", 4),
+		("Fred", 5),
+		("Ginny", 6),
+		("Harriet", 7),
+		("Ileana", 8),
+		("Joseph", 9),
+		("Kincaid", 10),
+		("Larry", 11),
+	]);
+	let student_idx = name2idx[student];
+
 	let mut plants_vector = Vec::new();
-	diagram_vector[0][student_idx[student]]
+
+	diagram_vector[0][student_idx]
 		.chars()
 		.for_each(|plant| plants_vector.push(plant_names[&plant]));
-	diagram_vector[1][student_idx[student]]
+	diagram_vector[1][student_idx]
 		.chars()
 		.for_each(|plant| plants_vector.push(plant_names[&plant]));
+
 	plants_vector
 }
